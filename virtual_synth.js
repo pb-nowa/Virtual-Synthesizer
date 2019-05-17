69bfb0c1f1c225145d5647336e46b99f2ab742bb
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-16 17:03:37 -0700

    Create gain class, is activated by make grain button

--- virtual_synth.js
+++ virtual_synth.js
@@ -1,26 +1,12 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
+const c = new AudioContext();
+const master = c.createGain();
+master.connect(c.destination);
 
 window.onload = () => {
-    const c = new AudioContext();
-    
-    const oscProp = {
-        type: "sine",
-        frequency: 440,
-    };
-
-    var audioNodeOptions = {
-        "length": 10,
-        "numberOfChannels": 2,
-        "sampleRate": 44100,
-    };
-    let osc, buffer, source;
-    let isPlaying = false;
-
-
-    const master = c.createGain();
-    master.connect(c.destination);
+    let buffer, source;
 
     const request = new XMLHttpRequest();
     request.open('GET', 'assets/audio/brahms_3_mvt3.mp3', true);
@@ -28,22 +14,16 @@ window.onload = () => {
     request.onload = function () {
         c.decodeAudioData(request.response, function(b) {
             buffer = b; //set the buffer
-            source = c.createBufferSource();
-            source.buffer = buffer;
-            source.connect(c.destination);
-            
+
             data = buffer.getChannelData(0);
             isloaded = true;
             console.log('loaded');
-            // var canvas1 = document.getElementById('canvas');
-            //initialize the processing draw when the buffer is ready
-            // var processing = new Processing(canvas1, waveformdisplay);
         }, function () {
             console.log('loading failed');
         });
     };
-    
     request.send();
+
 
 
     
@@ -54,35 +34,30 @@ window.onload = () => {
     
     const playButton = document.getElementById("play");
     playButton.addEventListener('click', function() {
-        if (c.state === 'suspended') {
-            c.resume();
-        } 
-        if (this.dataset.playing === 'false'){
-            source.start(c.currentTime);
-            this.dataset.playing = 'true';
+            // scheduled start, audio start time, sample length
+            source = c.createBufferSource();
+            source.buffer = buffer;
+            source.connect(master);
+            source.start(c.currentTime, 20, 6);
+            source.onended = () => {
+                console.log("file has ended");
+            };
             console.log('playing');
-            debugger
-        } else if (this.dataset.playing === 'true') {
-            source.stop(c.currentTime);
-            this.dataset.playing = 'false';
-            console.log("paused");
-        }
-    }, false);
-    // playButton.addEventListener('click', function() {
-    //     if (c.state === 'suspended') {
-    //         c.resume();
-    //     } 
-    //     if (this.dataset.playing === 'false'){
-    //         audioElement.play();
-    //         this.dataset.playing = 'true';
-    //     } else if (this.dataset.playing === 'true') {
-    //         audioElement.pause();
-    //         this.dataset.playing = 'false';
-    //     }
-    // }, false);
+    });
+
+    const grain = document.getElementById("grain");
+    grain.addEventListener('click', function() {
+            // scheduled start, audio start time, sample length
+            const grain = new Grain(buffer);
+            console.log(grain);
+            grain.source.start(c.currentTime, 20, 6);
+            grain.source.onended = () => {
+                console.log("file has ended");
+            };
+            console.log('playing');
+    });
 
     changeGain = () => {
-        //create gain node
         const masterGain = document.getElementById("master-gain");
         changeVolume(masterGain, master);
     };
@@ -105,24 +80,28 @@ window.onload = () => {
         play();
         play();
     };
-
 };
-// const play = (source) => {
-//     if (oscProp.playing) {
-//         osc.stop();
-//         oscProp.playing = false;
-//         console.log("stopped");
-//     } else {
-//         osc = c.createOscillator();
-//         osc.type = oscProp.type;
-//         osc.frequency.setValueAtTime(oscProp.frequency, c.currentTime);
-//         osc.connect(master);
-//         osc.start();
-//         oscProp.playing = true;
-//         console.log("playing");
-//     }
-// };
+
+class Grain {
+    constructor(buffer){
+        this.now = c.currentTime;
+        this.source = c.createBufferSource();
+        this.source.buffer = buffer;
+        this.attack = 2;
+        this.release = 2;
+
+        this.bus = c.createGain();
+        this.bus.connect(master);
+        this.source.connect(this.bus);
 
 
+        this.bus.gain.setValueAtTime(0, this.now);
+
+        // value, endtime
+        this.bus.gain.linearRampToValueAtTime(50, this.now + 2);
+        this.bus.gain.linearRampToValueAtTime(0, this.now + this.attack + this.release);
+
+    }
+}
 
 
