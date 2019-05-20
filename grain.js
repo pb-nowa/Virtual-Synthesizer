859fa287badf5ddd6ef623c93a5d8ca7edc64c0f
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-19 20:52:02 -0700

    Grains are produced in time with playback

--- grain.js
+++ grain.js
@@ -7,18 +7,18 @@ class Grain {
         this.attack = Math.random() * 0.3;
         this.sustain = Math.random() * 0.2;
         this.release = Math.random() * 0.05;
-        this.playbackSampleStart = 25; //where in the audio file to start playing
+        this.playbackSampleStart = startTime; //where in the audio file to start playing
 
         this.bus = this.context.createGain();
         this.source.connect(this.bus);
         this.bus.connect(masterbus);
         // this.bus.connect(convolver);
 
-        this.playGrain(startTime);
+        this.playGrain();
     }
 
-    playGrain(startTime) {
-        this.source.start(startTime, Math.random() * 0.5 + this.playbackSampleStart, this.attack + this.sustain + this.release);
+    playGrain() {
+        this.source.start(this.now, Math.random() * 0.5 + this.playbackSampleStart, this.attack + this.sustain + this.release);
         this.bus.gain.setValueAtTime(0, this.now);
         // value, endtime
         this.bus.gain.linearRampToValueAtTime(4, this.now + this.attack);
