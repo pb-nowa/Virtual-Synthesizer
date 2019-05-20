9074b038431c96484b3b0af3326ce62ef652d5d7
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-20 08:44:47 -0700

    Fix heroku maybe

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './virtual_synth.js',
     output: {
+        publicPath: '/',
         filename: 'bundle.js',
         path: path.resolve(__dirname)
     }
