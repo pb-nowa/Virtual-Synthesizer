2ff46bba7c4985e2af96643daf168d457ec4888d
PNowakowski <p.nowakowskimusic@gmail.com>
2019-07-03 23:03:43 -0700

    Refactor on window load and organize canvas code

--- virtual_synth.js
+++ virtual_synth.js
@@ -1,9 +1,13 @@
 import Grain from './grain.js';
 import { getBuffer } from './get_buffer';
 
+
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+
 let playing = false;
 let loaded = false;
+let isGranulating = false;
 let timer = 1;
 let timerId;
 let source;
@@ -18,17 +22,13 @@ let timeout;
 let color = [0, 212, 212];
 let colorClock = 0;
 
+
 const c = new AudioContext();
 const master = c.createGain();
 const masterbus = c.createGain();
 const reverbBus = c.createGain();
 let rad = 2;
 let Q = 1;
-
-const delay = new DelayNode(c, {
-    delayTime: 0.4,
-    maxDelayTime: 0.4,
-});
 
 const bandpass = new BiquadFilterNode(c, {
     type: 'bandpass',
@@ -43,11 +43,6 @@ const notch = new BiquadFilterNode(c, {
     Q: 100,
 });
 
-let lfo = new OscillatorNode(c, {
-    type: 'sine',
-    frequency: 1.5,
-});
-
 const analyser = new AnalyserNode(c, {
     fftSize: 2048,
     maxDecibles: -30,
@@ -55,11 +50,18 @@ const analyser = new AnalyserNode(c, {
     smoothingTimeConstant: 0.97
 });
 
+
 let convolver;
+
+function playGrains() {
+    const grain = new Grain(c, buffer, reverbBus, timer);
+    timeout = window.setTimeout(playGrains, Math.random() * 275);
+}
 
 async function setReverb() {
     convolver = c.createConvolver();
     convolver.buffer = await getBuffer(c, '/assets/audio/large_hall.wav');
+    
     reverbBus.connect(convolver)
     .connect(bandpass)
     .connect(notch)
@@ -70,84 +72,84 @@ async function setReverb() {
 setReverb();
 
 masterbus.connect(master);
-lfo.connect(master.gain);
-// lfo.start();
 master.connect(analyser);
 analyser.connect(c.destination);
 
-window.onload = () => {
-    let revBuffer;
+let revBuffer;
 
-    async function initBuffer() {
-        buffer = await getBuffer(c, '/assets/audio/reverie.mp3'); 
-        revBuffer = await getBuffer(c, '/assets/audio/reverie.mp3');
-        Array.prototype.reverse.call(revBuffer.getChannelData(0));
-        Array.prototype.reverse.call(revBuffer.getChannelData(1));
-        loaded = true;
-    }
+async function initBuffer() {
+    buffer = await getBuffer(c, '/assets/audio/reverie.mp3');
+    revBuffer = await getBuffer(c, '/assets/audio/reverie.mp3');
+    Array.prototype.reverse.call(revBuffer.getChannelData(0));
+    Array.prototype.reverse.call(revBuffer.getChannelData(1));
+    loaded = true;
+}
 
-    initBuffer();
-    
-    canvas.addEventListener('click', function(){
-        const bus = c.createGain();
-        if (!playing){
-            source = c.createBufferSource();
-            source.buffer = buffer;
-            source.start(c.currentTime, timer);
-            source.connect(bus);
-            bus.gain.linearRampToValueAtTime(1.2, c.currentTime + 3);
-            bus.connect(masterbus);
-            playing = true;
-            timerId = window.setInterval(() => {
-                if (!isMouseOver) timer++;
-            }, 1000);
-            document.getElementById('header-container').className = "header-container fade-out";
-        } else {
-            bus.gain.linearRampToValueAtTime(0, c.currentTime + 0.5);
-            source.stop(c.currentTime + 0.5);
-            playing = false;
-            window.clearInterval(timerId);
-            document.getElementById('header-container').className = "header-container fadeIn";
-        }
-    });
+initBuffer();
 
-    function setStart(){
-        const bus = c.createGain();
+
+
+
+const canvas = document.getElementById("sphere");
+
+const ctx = canvas.getContext('2d');
+let width = canvas.offsetWidth;
+let height = canvas.offsetHeight;
+
+canvas.addEventListener('click', function () {
+    const bus = c.createGain();
+
+    if (!playing) {
         source = c.createBufferSource();
         source.buffer = buffer;
         source.start(c.currentTime, timer);
         source.connect(bus);
-        bus.gain.linearRampToValueAtTime(1.2, c.currentTime + 1);
+
+        bus.gain.linearRampToValueAtTime(1.2, c.currentTime + 3);
         bus.connect(masterbus);
+
         playing = true;
+
         timerId = window.setInterval(() => {
-            timer++;
+            if (!isMouseOver) timer++;
         }, 1000);
+
+        document.getElementById('header-container').className = "header-container fade-out";
+
+    } else {
+        bus.gain.linearRampToValueAtTime(0, c.currentTime + 0.5);
+        source.stop(c.currentTime + 0.5);
+        playing = false;
+        window.clearInterval(timerId);
+        document.getElementById('header-container').className = "header-container fadeIn";
     }
+});
 
-    function playGrains() {
-        const grain = new Grain(c, revBuffer, reverbBus, timer);
+canvas.addEventListener('mousemove', (e) => {
+    const radius = 200;
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+
+function onResize() {
+    width = canvas.offsetWidth;
+    height = canvas.offsetHeight;
+    if (window.devicePixelRatio > 1) {
+        canvas.width = canvas.clientWidth * 2;
+        canvas.height = canvas.clientHeight * 2;
+        ctx.scale(2, 2);
+    } else {
+        canvas.width = width;
+        canvas.height = height;
     }
+}
 
- 
-    canvas.addEventListener('mousemove', (e) => {
-        const radius = 200;
-        mouse.x = e.x;
-        mouse.y = e.y;
-    });
+window.addEventListener('resize', onResize);
+onResize();
 
-    const aboutPage = document.getElementById("about-page");
-    aboutPage.addEventListener('click', e => {
-        aboutPage.className = "about-page fade-out";
-        aboutPage.childNodes.forEach(node => {
-            node.className += " fade-out";
-        });
-        document.getElementById('header-container').className += " fadeIn";
-    });
 
-};
 
-const canvas = document.getElementById("sphere");
 const sphereBack = document.getElementById("sphere-background");
 
 sphereBack.addEventListener('mouseenter', e => {
@@ -173,43 +175,13 @@ sphereBack.addEventListener('mousemove', (e) => {
     mouse.y = e.y;
 });
 
-let width = canvas.offsetWidth;
-let height = canvas.offsetHeight;
 
-const ctx = canvas.getContext('2d');
-
-function playGrains() {
-    const grain = new Grain(c, buffer, reverbBus, timer);
-    timeout = window.setTimeout(playGrains, Math.random() * 275);
-}
-
-let isGranulating = false;
-
-
-function onResize() {
-    width = canvas.offsetWidth;
-    height = canvas.offsetHeight;
-    // If the screen device has a pixel ratio over 1
-    // We render the canvas twice bigger to make it sharper (e.g. Retina iPhone)
-    if (window.devicePixelRatio > 1) {
-        canvas.width = canvas.clientWidth * 2;
-        canvas.height = canvas.clientHeight * 2;
-        ctx.scale(2, 2);
-    } else {
-        canvas.width = width;
-        canvas.height = height;
-    }
-}
-
-window.addEventListener('resize', onResize);
-onResize();
 
 
 let PERSPECTIVE = width * 0.8;
 let PROJECTION_CENTER_X = width / 2;
 let PROJECTION_CENTER_Y = height / 2;
 const PARTICLE_RADIUS = 1.6;
-// let GLOBE_RADIUS = width / 3;
 let GLOBE_RADIUS = 40;
 let particles = [];
 let repulsedParticles = [];
@@ -219,7 +191,6 @@ class Particle {
         this.analyser = analyser;
 
         this.theta = theta || Math.random() * 2 * Math.PI;
-        //multiply MathRandom by acos so that the partcles don't clump near the poles
         this.phi = phi || Math.acos((Math.random() * 2) - 1);
 
         this.x = x || 0;
@@ -231,7 +202,6 @@ class Particle {
         this.scaleProjected = z || 0;
         this.rad = rad || 0; 
         this.timer = timer || 0;
-
     }
 
     project() {
@@ -293,9 +263,8 @@ class Particle {
         ctx.fillStyle = `rgb(${color})`;
         ctx.fill();
     }
-
-
 }
+
 
 let density = 30;
 
@@ -315,26 +284,14 @@ function draw(ctx, img, x, y, w, h){
     }
 }
 
-let loadParticles = [ 
-    { idx: 1, saveRad: 1200, particles: [] },
-    { idx: 2, saveRad: 900, particles: [] },
-    { idx: 3, saveRad: 600, particles: [] },
-    { idx: 4, saveRad: 300, particles: [] }
-];
-
-let loadIdx = 0;
-let saveRad = 1200;
-let replenish = [];
-
-
-function render(ctx) {
+function render(params) {
+    const { ctx } = params;
     const r = 0;
-    let g = 212;// Math.floor((212 + colorClock) * 0.4 % 64) + 212 - 32;
-    let b = 212; //Math.floor((colorClock) * 0.5 % 64) + 212 - 32;
+    let g = 212;
+    let b = 212; 
     color = [r, g, b];
     ctx.clearRect(0, 0, width, height);
 
-    let loadRad = 1200;
     //play and pause button
     if (loaded) {
         if (playing) {
@@ -342,9 +299,7 @@ function render(ctx) {
         } else {
             draw(ctx, play, width / 2 - 75, height - 130, 150, 100);
         }
-    }
-    
-    if (loaded){
+ 
         density = 1300;
         if (!particles.length) {
             inside = false;
@@ -385,35 +340,7 @@ function render(ctx) {
                 }
             }  
         }
-    } else {
-    // load screen
-        // if (!loadParticles[0].particles.length) {
-        //     for (let j = 0; j  < loadParticles.length; j++) {
-        //         for (let i = 0; i < density; i++) {
-        //             loadParticles[j].particles.push(new Particle({ analyser, rad: loadParticles[j].saveRad }));
-        //         } 
-        //     }
-        // } else {
-        //     for (let j = 0; j < loadParticles.length; j++) {
-        //         const preParticles = Array.from(loadParticles[j].particles);
-        //         loadParticles[j].particles = [];
-        //         const zoomSpeed = 25;
-        //         loadParticles[j].saveRad = (loadParticles[j].saveRad <= 0) ? loadRad : loadParticles[j].saveRad - zoomSpeed;
-                
-        //         for (let i = 0; i < density; i++) {
-        //             loadParticles[j].particles.push(new Particle({
-        //                 analyser: analyser, 
-        //                 rad: loadParticles[j].saveRad,
-        //                 theta: preParticles[i].theta,
-        //                 phi: preParticles[i].phi,
-        //                 x: preParticles[i].x, 
-        //                 y: preParticles[i].y, 
-        //                 z: preParticles[i].z
-        //             }));
-        //         } 
-        //     } 
-        // }
-    }
+    } 
 
     
     for (let i = 0; i < particles.length; i++){
@@ -425,13 +352,7 @@ function render(ctx) {
         return dot1.sizeProjection - dot2.sizeProjection;
     });
 
-    if (!loaded) {
-        for (let j = 0; j < loadParticles.length; j++) {
-            for (let i = 0; i < loadParticles[j].particles.length; i++){
-                loadParticles[j].particles[i].draw();
-            }
-        }
-    } else {
+    if (loaded) {
         for (let i = 0; i < particles.length; i++) {
             particles[i].draw();
         }
@@ -447,11 +368,41 @@ function render(ctx) {
 
     // granulate();
     colorClock++;
-    window.requestAnimationFrame(() => render(ctx));
+    window.requestAnimationFrame(() => render({ ctx }));
 }
 
 function init() {
-    window.requestAnimationFrame(() => render(ctx));
+    window.requestAnimationFrame(() => render({ ctx }));
 }
+
+
+
+
+
+function setStart() {
+    const bus = c.createGain();
+    source = c.createBufferSource();
+    source.buffer = buffer;
+    source.start(c.currentTime, timer);
+    source.connect(bus);
+    bus.gain.linearRampToValueAtTime(1.2, c.currentTime + 1);
+    bus.connect(masterbus);
+    playing = true;
+    timerId = window.setInterval(() => {
+        timer++;
+    }, 1000);
+}
+
+
+
+const aboutPage = document.getElementById("about-page");
+aboutPage.addEventListener('click', e => {
+    aboutPage.className = "about-page fade-out";
+    aboutPage.childNodes.forEach(node => {
+        node.className += " fade-out";
+    });
+    document.getElementById('header-container').className += " fadeIn";
+});
+
 
 init();
